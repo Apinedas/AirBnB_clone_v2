baf4c2a3e727a984fd35839e5a45dde055cecef4
Alejandro Pineda Sánchez <apsanchez2549@gmail.com>
2022-05-23 00:17:01 -0500

    feat: Added 1

---
+++ 1-pack_web_static.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+'''Packs all needed files on a tgz file'''
+
+
+def do_pack():
+    '''Packs all HBnB static on a tgz file'''
+    import datetime
+    from fabric.api import local
+    now = datetime.datetime.now()
+
+    name = 'web_static_{}{}{}{}{}{}.tgz'.format(now.year, now.month, now.day, now.hour, now.minute, now.second)
+    result = local("tar -cvzf versions/{} web_static".format(name))
+    if result.succeeded:
+        return "versions/{}".format(name)
+    return None
