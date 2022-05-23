b29a11ddc39b2735406f3f2d491f50880eab841c
Alejandro Pineda Sánchez <apsanchez2549@gmail.com>
2022-05-22 21:34:08 -0500

    feat: Added 0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -9,7 +9,7 @@ sudo touch /data/web_static/releases/test/index.html
 sudo chmod 666 /data/web_static/releases/test/index.html
 echo -e "<html>\n\t<head>\n\t</head>\n\t<body>\n\t\tHolberton School\n\t</body>\n</html>" >> /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test /data/web_static/current
-sudo chown -R ubuntu:ubuntu /data/
+sudo chown -hR ubuntu:ubuntu /data/
 new_string="server_name _;\n\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}"
 sudo sed -i "0,/server_name _;/{s|server_name _;|$new_string|}" /etc/nginx/sites-available/default
 sudo service nginx restart
