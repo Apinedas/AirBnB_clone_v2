03ec10d9a129f24d08d7780fb7639062ad5fd970
Alejandro Pineda Sánchez <apsanchez2549@gmail.com>
2022-05-23 17:08:35 -0500

    feat: Added 3

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -5,6 +5,7 @@
 from fabric.api import *
 env.hosts = ['34.75.140.247', '54.198.208.252']
 
+
 def do_deploy(archive_path):
     '''fuction to deploy'''
     from os.path import exists
@@ -15,11 +16,14 @@ def do_deploy(archive_path):
     put(archive_path, "/tmp/")
     filename = archive_path.split('/')[1]
     run("mkdir -p /data/web_static/releases/{}".format(filename[0:-4]))
-    run("tar -xzf {} -C {}".format("/tmp/" + filename, "/data/web_static/releases/" + filename[0:-4]))
-    run("mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}".format(filename[0:-4], filename[0:-4]))
+    run("tar -xzf {} -C {}".format("/tmp/" + filename,
+        "/data/web_static/releases/" + filename[0:-4]))
+    run("mv /data/web_static/releases/{}/web_static/*\
+        /data/web_static/releases/{}".format(filename[0:-4], filename[0:-4]))
     run("rm -rf /data/web_static/releases/{}/web_static")
     run("rm -f /tmp/{}".format(filename))
     run("rm /data/web_static/current")
-    run('ln -sf /data/web_static/releases/{} /data/web_static/current'.format(filename[0:-4]))
+    run('ln -sf /data/web_static/releases/{}\
+        /data/web_static/current'.format(filename[0:-4]))
     sudo('service nginx restart')
     return True
