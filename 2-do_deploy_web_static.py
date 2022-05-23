315afb549e834623d13ed13e204aeab2ca3c2f9d
Alejandro Pineda Sánchez <apsanchez2549@gmail.com>
2022-05-23 15:53:58 -0500

    fix: Changed 2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -16,6 +16,8 @@ def do_deploy(archive_path):
     filename = archive_path.split('/')[1]
     run("mkdir -p /data/web_static/releases/{}".format(filename[0:-4]))
     run("tar -xzf {} -C {}".format("/tmp/" + filename, "/data/web_static/releases/" + filename[0:-4]))
+    run("mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}".format(filename[0:-4], filename[0:-4]))
+    run("rm -rf /data/web_static/releases/{}/web_static")
     run("rm -f /tmp/{}".format(filename))
     run("rm /data/web_static/current")
     run('ln -sf /data/web_static/releases/{} /data/web_static/current'.format(filename[0:-4]))
