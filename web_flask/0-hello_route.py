7d9a8b1bab8b8a906e76bea14cc04c56e18b488e
Alejandro Pineda Sánchez <apsanchez2549@gmail.com>
2022-05-24 21:53:00 -0500

    feat: Added 1

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -8,6 +8,7 @@ app = Flask(__name__)
 
 @app.route("/", strict_slashes=False)
 def hello_hbnb():
+    '''First function. Prints on /'''
     return "Hello HBNB!"
 
 
--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -8,11 +8,13 @@ app = Flask(__name__)
 
 @app.route("/", strict_slashes=False)
 def hello_hbnb():
+    '''First function. Prints on /'''
     return "Hello HBNB!"
 
 
 @app.route("/hbnb", strict_slashes=False)
 def hello_hbnb():
+    '''Second function. Prints on /hbnb'''
     return "HBNB"
 
 
