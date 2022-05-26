ebaebfd53704412dd4ded44c5421bd01b53ffadb
Alejandro Pineda Sánchez <apsanchez2549@gmail.com>
2022-05-25 23:08:02 -0500

    fix: Style changes

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -7,10 +7,12 @@ from models import storage
 from models.state import State
 app = Flask(__name__)
 
+
 @app.teardown_appcontext
 def teardown_close(self):
     "Closses sqlalchemy session"
     storage.close()
+
 
 @app.route('/states_list', strict_slashes=False)
 def states_list():
@@ -20,6 +22,7 @@ def states_list():
     for state in state_dict.values():
         ret_list.append(state)
     return render_template('7-states_list.html', st_list=ret_list)
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
