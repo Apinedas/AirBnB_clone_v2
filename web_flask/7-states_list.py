cacfa6e68deb377a9c38714ae53f496eccf29c81
Alejandro Pineda Sánchez <apsanchez2549@gmail.com>
2022-05-25 23:07:11 -0500

    fix: Changed template on 7

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -3,18 +3,19 @@
 
 
 from flask import Flask, render_template
-from models import storage, state
+from models import storage
+from models.state import State
 app = Flask(__name__)
 
 @app.teardown_appcontext
-def teardown_close():
+def teardown_close(self):
     "Closses sqlalchemy session"
     storage.close()
 
 @app.route('/states_list', strict_slashes=False)
 def states_list():
     """Returns a HTML with states list"""
-    state_dict = storage.all(state.State)
+    state_dict = storage.all(State)
     ret_list = []
     for state in state_dict.values():
         ret_list.append(state)
